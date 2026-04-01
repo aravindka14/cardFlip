7b88dda98adb2555c96a2afc0692091c006b09a5
aravindka14 <aravindka14@gmail.com>
2026-04-01 18:01:35 +0530

    login

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,39 @@ import TopBar from "../components/layout/TopBar";
 const Home = () => {
   return (
     <>
-      <div className="flex h-screen overflow-hidden">
+      <div className="h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-[350px] bg-white p-8 rounded-2xl shadow-lg">
+        
+        <h1 className="text-3xl font-serif text-center mb-6">CardFlip</h1>
+
+        <form className="flex flex-col gap-4">
+          
+          <input
+            type="email"
+            placeholder="Email"
+            className="border px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            className="border px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+          />
+
+          <button
+            type="submit"
+            className="bg-black text-white py-2 rounded-lg hover:bg-gray-800 transition"
+          >
+            Login
+          </button>
+
+        </form>
+
+
+      </div>
+    </div>
+
+      {/* <div className="flex h-screen overflow-hidden">
         <SideBar />
         <div className="flex flex-col flex-grow">
           <TopBar />
@@ -13,7 +45,7 @@ const Home = () => {
             <Outlet />
           </main>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
