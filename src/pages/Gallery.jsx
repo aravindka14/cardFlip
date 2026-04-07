27ba336f9fce10bdc9cd1ca42a52fdab2494a18d
aravindka14 <aravindka14@gmail.com>
2026-04-07 14:46:25 +0530

    edits

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,30 +1,22 @@
-
 import React, { useEffect, useRef, useState } from "react";
 import { useGetGallery } from "../queries/gallery/useGalleryQueries";
 import PhotoCard from "../components/PhotoCard";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 const Gallery = () => {
+  const limit = 20;
   const [page, setPage] = useState(1);
   const [photos, setPhotos] = useState([]);
-  const [hasMore, setHasMore] = useState(true);
-  const appendedPages = useRef(new Set());
-
-  const { data } = useGetGallery(page);
+  const { data } = useGetGallery(page, limit);
 
   useEffect(() => {
-    if (data && !appendedPages.current.has(page)) {
-      appendedPages.current.add(page);
-      if (data.length < 20) {
-        setHasMore(false);
-      }
+    if (data) {
       setPhotos((prev) => [...prev, ...data]);
     }
   }, [data, page]);
 
   const fetchMore = () => {
     setPage((prev) => prev + 1);
-    console.log("next page");
   };
 
   return (
@@ -32,7 +24,7 @@ const Gallery = () => {
       dataLength={photos.length}
       loader={<h4>Loading...</h4>}
       next={fetchMore}
-      hasMore={hasMore}
+      hasMore={true}
       scrollableTarget="scrollableDiv"
     >
       <div className="p-9 grid grid-cols-4 gap-10">
--- src/queries/gallery/useGalleryQueries.js
+++ src/queries/gallery/useGalleryQueries.js
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { interceptor } from "../interceptor";
 
-export const useGetGallery = (page =1) => {
+export const useGetGallery = (page, limit) => {
   return useQuery({
     queryKey: ["gallery", page],
     queryFn: async () => {
       const res = await interceptor.get(
-        `/photos?_limit=20&_page=${page}`
+        `/photos?_limit=${limit}&_page=${page}`
       );
       return res.data;
     },
