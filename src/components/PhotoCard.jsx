b04a39f286fd5d710cf735f793bed0b69894227b
aravindka14 <aravindka14@gmail.com>
2026-04-07 18:16:14 +0530

    edits

--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -1,21 +1,23 @@
-import React, { useState } from "react";
-import { LuImageOff } from "react-icons/lu";
-
-
-const PhotoCard = ({title , url }) => {
-  const [error, setError] = useState(false);
+const PhotoCard = ({ title, url }) => {
   return (
-    <div className="border min-h-[300px] w-[250px] rounded-lg bg-gray-100 border-gray-200 flex flex-col items-center p-4 gap-10">
-      <p className="text-xl font-semibold text-center text-gray-600">{title}</p>
-      {error ? (
-        <LuImageOff size={40} />
-      ) : (
-        <img
-          src={url}
-          alt={<LuImageOff />}
-          onError={() => setError(true)}
-        />
-      )}
+    <div className="border min-h-[300px]   rounded-lg bg-gray-100 border-gray-200 flex flex-col items-center p-4 gap-10">
+      <div className="min-h-18">
+        <p className="text-xl font-semibold text-center text-gray-600 line-clamp-2">
+          {title}
+        </p>
+      </div>
+
+      <img
+        src="https://placehold.co/600x400/000000/FFF"
+        alt={title}
+        onError={(e) => {
+          console.log(e, ";;;;;;;;;;;");
+
+          e.preventDefault();
+          e.target.src =
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGZjYKPjVrCS_uKmuUXIkYNXPA3x0q_Y-hYQ&s";
+        }}
+      />
     </div>
   );
 };
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -22,12 +22,17 @@ const Gallery = () => {
   return (
     <InfiniteScroll
       dataLength={photos.length}
-      loader={<h4>Loading...</h4>}
+      loader={
+        <div className="col-span-4 flex justify-center py-6">
+          <div className="w-6 h-6 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
+        </div>
+      }
       next={fetchMore}
       hasMore={true}
+      scrollThreshold={1}
       scrollableTarget="scrollableDiv"
     >
-      <div className="p-9 grid grid-cols-4 gap-10">
+      <div className="p-9 grid xs:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-10">
         {photos.map((card) => (
           <PhotoCard key={card.id} {...card} />
         ))}
