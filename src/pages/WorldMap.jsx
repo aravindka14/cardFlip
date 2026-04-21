8452deb9e2846fd0998103453a92d976a2c3d44c
aravindka14 <aravindka14@gmail.com>
2026-04-21 14:45:23 +0530

    world-map

--- src/pages/WorldMap.jsx
+++ src/pages/WorldMap.jsx
@@ -1,21 +1,57 @@
-import React from 'react'
+import React from "react";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import WorldMap from "react-svg-worldmap";
 
 const WorldMaps = () => {
-    const data = [
+  const data = [
     { country: "IN", value: 100 },
     { country: "US", value: 200 },
     { country: "BR", value: 50 },
   ];
-  return (
-    <div className="w-full h-[500px]">
-      <WorldMap
-        color="blue"
-        data={data}
-        size="responsive"
-      />
-    </div>
-  )
-}
 
-export default WorldMaps
+ const stylingFunction = (context) => {
+  const value = context.countryValue || 0;
+
+  // simple gradient logic
+  let fillColor = "#d4d4d4"; // default (gray)
+
+  if (value > 150) fillColor = "#000000";     // dark blue
+  else if (value > 100) fillColor = "#292929"; // medium blue
+  else if (value > 50) fillColor = "#242424";  // light blue
+
+  return {
+    fill: fillColor,
+    stroke: "#524949",
+    strokeWidth: 0.5,
+    cursor: "pointer",
+  };
+};
+  return (
+    <div className="w-full flex items-center justify-center rounded-lg bg-white overflow-hidden">
+      <TransformWrapper
+        initialScale={1}
+        minScale={1}
+        maxScale={5}
+        doubleClick={{ disabled: true }}
+      >
+        <TransformComponent>
+          <WorldMap
+            size={1070}
+            color="blue"
+            data={data}
+            // richInteraction={true}
+            // backgroundColor={"#ffffff"}
+            // borderColor={"#ffffff"}
+            color={"#060808"}
+            tooltipBgColor={"#020205"}
+            frameColor={"#ffffff"}
+            // zoom={}
+            styleFunction={stylingFunction}
+          />
+        </TransformComponent>
+      </TransformWrapper>
+    </div>
+  );
+};
+
+export default WorldMaps;
