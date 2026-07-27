ebe8f8b395f2d512aeaee1a885ea777a0cb7e4cb
aravindka14 <aravindka14@gmail.com>
2026-07-27 18:32:03 +0530

    final edits

--- src/queries/interceptor.js
+++ src/queries/interceptor.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3000";
+// const baseURL = "http://localhost:3000";
+const baseURL = "https://cardflip-backend.onrender.com";
+
 
 export const interceptor = axios.create({
   baseURL,
